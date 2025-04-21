32d46e0267e606c0481fc3b9dd52da75db09830e
Shevchenko <mc_vshevchenko25@mcjvs2.org>
2025-04-21 13:22:33 -0400

    Fixed print statements

--- Scripts/SamsBody3D.gd
+++ Scripts/SamsBody3D.gd
@@ -27,26 +27,20 @@ func _ready():
 func _physics_process(delta):
 	target = get_tree().get_first_node_in_group("player")
 	if target:
-		print("Player Position in _physics_process:", target.global_transform.origin)
 		move_and_attack(delta) # No need to pass target again, class-level is updated
 	else:
 		print("Player not found!")
 
 func move_and_attack(delta):
 	if target:
-		print("Target in move_and_attack:", target.global_transform.origin)
 		var direction = (target.global_transform.origin - global_transform.origin).normalized()
-		print("Direction:", direction)
 		velocity = direction * move_speed
-		print("Velocity:", velocity)
 		move_and_slide()
 
 		attack_timer += delta
 		if global_position.distance_to(target.global_position) <= attack_range:
 			# Your attack logic here
 			pass
-	else:
-		print("Target is null in move_and_attack!")
 
 func die():
 	emit_signal("enemy_died", self)
